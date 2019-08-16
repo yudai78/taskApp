9ac41ce1973384f3a3684fe9f6ef5ad2c1245bc1
yudai <kabuyouaka@gmail.com>
2019-08-16 18:51:44 +0900

    pod install と.gitignoreまで実施

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 阿部祐大 on 2019/08/16.
+//  Copyright © 2019 yudai.abe. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+}
